a8afd24a5a040560af96068b1381d83f6e80c0a2
aman0iz0back <dr.amans8932@gmail.com>
2025-07-30 11:34:35 +0530

    changes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,41 +1,11 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "./src/**/*.{js,jsx,ts,tsx}",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-//   darkMode: 'media',
-//   extend: {
-//   keyframes: {
-//     shake: {
-//       '0%, 100%': { transform: 'translateX(0)' },
-//       '25%': { transform: 'translateX(-6px)' },
-//       '50%': { transform: 'translateX(6px)' },
-//       '75%': { transform: 'translateX(-4px)' }
-//     },
-//     success: {
-//       '0%': { transform: 'scale(1)' },
-//       '50%': { transform: 'scale(1.03)' },
-//       '100%': { transform: 'scale(1)' }
-//     }
-//   },
-//   animation: {
-//     shake: 'shake 0.4s ease-in-out',
-//     success: 'success 0.4s ease-in-out'
-//   }
-// },
 
-// }
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: 'media', // Enables automatic dark mode based on system
+  darkMode: 'media', 
   theme: {
     extend: {
       keyframes: {
